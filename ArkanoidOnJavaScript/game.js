79f4abecf301840cdded8de4759785b33c08b21c
hel1_yeah <hel1_yeah@ukr.net>
2022-01-09 19:00:07 +0200

    feat: blocks

--- ArkanoidOnJavaScript/game.js
+++ ArkanoidOnJavaScript/game.js
@@ -2,10 +2,14 @@ let game = {
   ctx: null,
   platform: null,
   ball: null,
+  blocks: [],
+  rows: 4,
+  cols: 8,
   sprites: {
     background: null,
     ball: null,
     platform: null,
+    block: null,
   },
 
   init() {
@@ -15,19 +19,20 @@ let game = {
   preload(callback) {
     let loadet = 0;
     let required = Object.keys(this.sprites).length;
-    console.log(required);
+
+    let onLoad = () => {
+      ++loadet;
+      console.log();
+      if (loadet >= required) {
+        callback();
+      }
+    };
+
     for (const key in this.sprites) {
       if (Object.hasOwnProperty.call(this.sprites, key)) {
         this.sprites[key] = new Image();
         this.sprites[key].src = `./img/${key}.png`;
-
-        this.sprites[key].addEventListener('load', () => {
-          ++loadet;
-          console.log();
-          if (loadet >= required) {
-            callback();
-          }
-        });
+        this.sprites[key].addEventListener('load', onLoad);
       }
     }
   },
@@ -37,17 +42,30 @@ let game = {
       this.sprites.ball,
       0,
       0,
-      this.ball.w,
-      this.ball.h,
+      this.ball.width,
+      this.ball.height,
       this.ball.x,
       this.ball.y,
-      this.ball.w,
-      this.ball.h
+      this.ball.width,
+      this.ball.height
     );
 
     this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
-  },
 
+    for (const block of this.blocks) {
+      this.ctx.drawImage(this.sprites.block, block.x, block.y);
+    }
+  },
+  created() {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
+        this.blocks.push({
+          x: 65 * col + 65,
+          y: 25 * row + 35,
+        });
+      }
+    }
+  },
   run() {
     window.requestAnimationFrame(() => {
       this.render();
@@ -57,6 +75,7 @@ let game = {
   start: function () {
     this.init();
     this.preload(() => {
+      this.created();
       this.run();
     });
   },
@@ -65,8 +84,8 @@ let game = {
 game.ball = {
   x: 320,
   y: 280,
-  w: 20,
-  h: 20,
+  width: 20,
+  height: 20,
 };
 game.platform = {
   x: 280,
