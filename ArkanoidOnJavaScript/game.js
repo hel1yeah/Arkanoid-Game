4a89d852c96f0af7e55bf29210266ac5768b01fe
hel1_yeah <hel1_yeah@ukr.net>
2022-01-10 19:53:03 +0200

    fix: deleted block

--- ArkanoidOnJavaScript/game.js
+++ ArkanoidOnJavaScript/game.js
@@ -24,6 +24,7 @@ let game = {
     this.ctx = document.getElementById('mycanvars').getContext('2d');
     this.setEvents();
   },
+
   setEvents() {
     window.addEventListener('keydown', (e) => {
       if (e.code === KEYS.SPACE) {
@@ -36,13 +37,13 @@ let game = {
       this.platform.stop();
     });
   },
+
   preload(callback) {
     let loadet = 0;
     let required = Object.keys(this.sprites).length;
 
     let onLoad = () => {
       ++loadet;
-      console.log();
       if (loadet >= required) {
         callback();
       }
@@ -56,6 +57,7 @@ let game = {
       }
     }
   },
+
   render() {
     this.ctx.clearRect(0, 0, this.width, this.height);
     this.ctx.drawImage(this.sprites.background, 0, 0);
@@ -74,24 +76,47 @@ let game = {
     this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
     this.renderBlocks();
   },
+
   renderBlocks() {
     for (const block of this.blocks) {
-      this.ctx.drawImage(this.sprites.block, block.x, block.y);
+      if (block.active) {
+        this.ctx.drawImage(this.sprites.block, block.x, block.y);
+      }
     }
   },
+
   created() {
     for (let row = 0; row < this.rows; row++) {
       for (let col = 0; col < this.cols; col++) {
         this.blocks.push({
+          active: true,
+          width: 60,
+          height: 20,
           x: 65 * col + 65,
           y: 25 * row + 35,
         });
       }
     }
   },
+
   update() {
     this.platform.move();
     this.ball.move();
+    this.collideBlocks();
+    this.collidePlatform();
+  },
+
+  collideBlocks() {
+    for (const block of this.blocks) {
+      if (block.active && this.ball.collide(block)) {
+        this.ball.bumbBlock(block);
+      }
+    }
+  },
+  collidePlatform() {
+    if (this.ball.collide(this.platform)) {
+      this.ball.bumbPlatform(this.platform);
+    }
   },
   run() {
     window.requestAnimationFrame(() => {
@@ -132,12 +157,37 @@ game.ball = {
     this.dy = -this.velocity;
     this.dx = game.random(-this.velocity, +this.velocity);
   },
+  collide(elem) {
+    let x = this.x + this.dx;
+    let y = this.y + this.dy;
+    if (
+      y < elem.y + elem.height &&
+      y + this.height > elem.y &&
+      x + this.width > elem.x &&
+      x < elem.x + elem.width
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  bumbBlock(block) {
+    this.dy *= -1;
+    block.active = false;
+  },
+  bumbPlatform(platform) {
+    this.dy *= -1;
+    let touchX = this.x + this.width / 2;
+    this.dx = this.velocity * platform.gettTouchOffset(touchX);
+  },
 };
 game.platform = {
   velocity: 5,
   dx: 0,
   x: 280,
   y: 300,
+  width: 100,
+  height: 14,
   ball: game.ball,
   move() {
     if (this.dx) {
@@ -163,6 +213,13 @@ game.platform = {
   },
   stop() {
     this.dx = 0;
+  },
+  gettTouchOffset(x) {
+    let diff = this.x + this.width - x;
+    let offset = this.width - diff;
+
+    result = (2 * offset) / this.width;
+    return result - 1;
   },
 };
 
